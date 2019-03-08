59af670523c5267f3810f621c9d9bef89ad9ea49
franklinjff <32177920+franklinjff@users.noreply.github.com>
2019-03-08 13:44:58 -0700

    Update LB WAF ACL Associations
    Update LB WAF ACL Associations

--- aws/Jenkins_proj-master/waf_conf/waf_acl_associate.tf
+++ aws/Jenkins_proj-master/waf_conf/waf_acl_associate.tf
@@ -6,8 +6,15 @@ provider "aws" {
 data "aws_alb" "ext-alb" {
   name = "ext-alb"
 }
+data "aws_alb" "native-alb" {
+  name = "native-alb"
+}
 
 resource "aws_wafregional_web_acl_association" "ext-alb" {
   resource_arn = "${data.aws_alb.ext-alb.arn}"
+  web_acl_id   = "${aws_wafregional_web_acl.wafregional_acl.id}"
+}
+resource "aws_wafregional_web_acl_association" "native-alb" {
+  resource_arn = "${data.aws_alb.native-alb.arn}"
   web_acl_id   = "${aws_wafregional_web_acl.wafregional_acl.id}"
 }
