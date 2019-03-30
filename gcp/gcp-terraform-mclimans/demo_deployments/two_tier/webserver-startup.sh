370279268e38a7b4a34792987bbf6bbfedf45869
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2019-03-30 18:26:24 -0400

    Delete webserver-startup.sh

--- gcp/gcp-terraform-mclimans/demo_deployments/two_tier/webserver-startup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-apt-get update
-apt-get install -y apache2 php7. libapache2-mod-php7.
-rm -f /var/www/html/index.html
-wget -O /var/www/html/index.php https://raw.githubusercontent.com/jasonmeurer/azure-appgw-2fw/master/showheaders.php
-systemctl restart apache2
