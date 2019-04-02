16dfb95c9730b46ec7bccde83cce1e28c8f64386
franklinjff <32177920+franklinjff@users.noreply.github.com>
2019-04-01 20:29:04 -0600

    Update Run.sh location
    Update Run.sh location

--- aws/Jenkins_proj-master/WebInDeploy/kali-instance.tf
+++ aws/Jenkins_proj-master/WebInDeploy/kali-instance.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "kali" {
           "cd /var/tmp\n",
           "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/.temp/Dockerfile\n",
           "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/.temp/docker-compose.yml\n",
-          "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/attaker/run.sh\n",
+          "wget https://github.com/wwce/terraform/blob/master/aws/Jenkins_proj-master/attacker/run.sh\n",
           "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/attacker/auto-sploit.sh\n",
           "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/exp-server.py\n",
           "docker-compose build\n",
