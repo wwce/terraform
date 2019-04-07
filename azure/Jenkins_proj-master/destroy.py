a948d3f4c5880bc0260d0f95cf5b34e58f684795
panwce <46921602+panwce@users.noreply.github.com>
2019-04-07 19:52:52 +0100

    Initial commit

---
+++ azure/Jenkins_proj-master/destroy.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+"""
+Paloaltonetworks Deploy_Jenkins_Hack_Demo.py
+
+This software is provided without support, warranty, or guarantee.
+Use at your own risk.
+"""
+
+'''
+Destroys resources post testing
+
+Usage
+
+python deploy.py -u <fwusername> -p <fwpassword> -a <Attack RG Name>, '-g' <RG name>
+
+'''
+
+import logging
+import ssl
+import urllib
+import xml.etree.ElementTree as et
+import xml
+import time
+import argparse
+import json
+import sys
+
+from pandevice import firewall
+from pandevice import updater
+from  python_terraform import Terraform
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+def main(username, password, attack_rg_name, rg_name):
+
+    username = username
+    password = password
+
+    WebInDeploy_vars = {
+        'Admin_Username': username,
+        'Admin_Password': password,
+        'Attack_RG_Name': attack_rg_name,
+        'RG_Name': rg_name
+    }
+
+    WebInBootstrap_vars = {
+        'Admin_Username': username,
+        'Admin_Password': password
+    }
+
+    albDns = ''
+    nlbDns = ''
+    fwMgt = ''
+
+    # Set run_plan to TRUE is you wish to run terraform plan before apply
+    run_plan = False
+    deployment_status = {}
+    kwargs = {"auto-approve": True}
+
+    #
+    # Destroy Infrastructure
+    #
+    tf = Terraform(working_dir='./WebInDeploy')
+
+    if run_plan:
+        print('Calling tf.plan')
+        tf.plan(capture_output=False)
+
+    return_code1, stdout, stderr = tf.cmd('destroy',var = WebInDeploy_vars, capture_output=False,**kwargs)
+    #return_code1 =0
+    print('Got return code {}'.format(return_code1))
+
+    if return_code1 != 0:
+        logger.info("Failed to destroy build ")
+
+
+        exit()
+    else:
+
+        logger.info("Destroyed WebInDeploy ")
+
+
+
+
+
+
+    tf = Terraform(working_dir='./WebInBootstrap')
+
+    if run_plan:
+        print('Calling tf.plan')
+        tf.plan(capture_output=False)
+
+    return_code1, stdout, stderr = tf.cmd('destroy',var = WebInBootstrap_vars,capture_output=False,**kwargs)
+    #return_code1 =0
+    print('Got return code {}'.format(return_code1))
+
+    if return_code1 != 0:
+        logger.info("WebInBootstrap destroyed")
+        deployment_status = {'WebInDeploy': 'Fail'}
+
+        exit()
+    else:
+        deployment_status = {'WebInDeploy':'Success'}
+        exit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get Terraform Params')
+    parser.add_argument('-u', '--username', help='Firewall Username', required=True)
+    parser.add_argument('-p', '--password', help='Firewall Password', required=True)
+    parser.add_argument('-a', '--attack_rg_name', help='Attack RG Name', required=True)
+    parser.add_argument('-g', '--rg_name', help='RG Name', required=True)
+
+    args = parser.parse_args()
+    username = args.username
+    password = args.password
+    attack_rg_name = args.attack_rg_name
+    rg_name = args.rg_name
+
+    main(username, password, attack_rg_name, rg_name)
