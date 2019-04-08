e0438ac087538cf119787e8e90ef89ed59621cef
panwce <46921602+panwce@users.noreply.github.com>
2019-04-08 17:45:28 +0100

    Update destroy.py

--- azure/Jenkins_proj-master/destroy.py
+++ azure/Jenkins_proj-master/destroy.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 """
-Paloaltonetworks Deploy_Jenkins_Hack_Demo.py
+# Copyright (c) 2018, Palo Alto Networks
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-This software is provided without support, warranty, or guarantee.
-Use at your own risk.
+# Author: Justin Harris jharris@paloaltonetworks.com
 """
 
 '''
@@ -11,7 +22,7 @@ Destroys resources post testing
 
 Usage
 
-python deploy.py -u <fwusername> -p <fwpassword> -a <Attack RG Name>, '-g' <RG name>
+python deploy.py -u <fwusername> -p <fwpassword>
 
 '''
 
