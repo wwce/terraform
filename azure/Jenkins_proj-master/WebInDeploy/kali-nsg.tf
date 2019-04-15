9b692cdc3d2c769a07bfeba1c0e7471504a58e0d
Nathan <nembery@gmail.com>
2019-04-15 16:41:03 -0400

    allow port 5000 to attacker

--- azure/Jenkins_proj-master/WebInDeploy/kali-nsg.tf
+++ azure/Jenkins_proj-master/WebInDeploy/kali-nsg.tf
@@ -26,6 +26,18 @@ resource "azurerm_network_security_group" "Attack_NSG" {
     source_address_prefix      = "*"
     destination_address_prefix = "${var.Attack_IP}"
   }
+
+  security_rule {
+    name                       = "Allow-FW-5000"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "5000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "${var.Attack_IP}"
+  }
 }
 resource "azurerm_subnet_network_security_group_association" "attackgroup" {
   subnet_id                 = "${azurerm_subnet.attacker.id}"
--- azure/Jenkins_proj-master/WebInFWConf/vars.testing
+++
@@ -1,8 +0,0 @@
-
-variable "mgt-ipaddress-fw1" {}
-variable "untrust-ipaddress-fw1" {}
-variable "WebSrv1_IP" {}
-
-variable "lb-ip" {}
-variable "username" {}
-variable "password" {}
