4d4d925e61fb04b88156a7cb9d2400127e2c9301
franklinjff <32177920+franklinjff@users.noreply.github.com>
2019-04-16 13:19:44 -0600

    Update initialize_attacker1.sh
    update dockerfile location

--- azure/Jenkins_proj-master/WebInDeploy/scripts/initialize_attacker1.sh
+++ azure/Jenkins_proj-master/WebInDeploy/scripts/initialize_attacker1.sh
@@ -4,8 +4,8 @@ apt-get update
 apt install docker.io python3-pip build-essential libssl-dev libffi-dev -y --force-yes
 pip3 install docker-compose
 cd /var/tmp
-wget https://raw.githubusercontent.com/nembery/terraform-1/master/azure/Jenkins_proj-master/.temp/Dockerfile
-wget https://raw.githubusercontent.com/nembery/terraform-1/master/azure/Jenkins_proj-master/.temp/docker-compose.yml
+wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/attacker/Dockerfile
+wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/attacker/docker-compose.yml
 wget https://github.com/wwce/terraform/blob/master/azure/Jenkins_proj-master/attacker/run.sh
 wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/attacker/auto-sploit.sh
 wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/exp-server.py
