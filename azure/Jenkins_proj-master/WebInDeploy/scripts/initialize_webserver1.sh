2449db0fcae77696f3c658d27297acce4de104d1
franklinjff <32177920+franklinjff@users.noreply.github.com>
2019-04-16 11:11:49 -0600

    Update initialize_webserver1.sh
    update docker files to azure git repository

--- azure/Jenkins_proj-master/WebInDeploy/scripts/initialize_webserver1.sh
+++ azure/Jenkins_proj-master/WebInDeploy/scripts/initialize_webserver1.sh
@@ -4,8 +4,8 @@ apt-get update
 apt install docker.io python3-pip build-essential libssl-dev libffi-dev -y --force-yes
 pip3 install docker-compose
 cd /var/tmp
-wget https://jenkins-test-vuln.s3-us-west-2.amazonaws.com/Dockerfile
-wget https://jenkins-test-vuln.s3-us-west-2.amazonaws.com/docker-compose.yml
-wget https://jenkins-test-vuln.s3-us-west-2.amazonaws.com/jenkins.sh
+wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/jenkins/Dockerfile
+wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/jenkins/docker-compose.yml
+wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/jenkins/jenkins.sh
 docker-compose build
 docker-compose up -d
