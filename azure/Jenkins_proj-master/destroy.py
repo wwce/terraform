95f4c02487e193822ea0e01a65aa3e4eece77053
jharris10 <jharris@paloaltonetworks.com>
2019-04-29 11:24:59 +0100

    Added state file deletion and resource group deletion

--- azure/Jenkins_proj-master/destroy.py
+++ azure/Jenkins_proj-master/destroy.py
@@ -1,121 +1,88 @@
-#!/usr/bin/env python3
-"""
-# Copyright (c) 2018, Palo Alto Networks
-#
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-# Author: Justin Harris jharris@paloaltonetworks.com
-
-Usage:
-git
-python destroy.py
-
-"""
-
+from azure.cli.core import get_default_cli
+import sys
+import tempfile
 import argparse
 import logging
+import subprocess
+import os
 
 from python_terraform import Terraform
 
 logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter('%(levelname)-8s %(message)s')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
-def main(username, password):
-    username = username
-    password = password
+#
+# Usage azure_login.py -g rgname
+#
 
-    WebInDeploy_vars = {
-        'Admin_Username': username,
-        'Admin_Password': password
-    }
+sys.sterr = sys.stdout
 
-    WebInBootstrap_vars = {
-        'Admin_Username': username,
-        'Admin_Password': password
-    }
+print('Logging in to Azure using device code')
 
-    albDns = ''
-    nlbDns = ''
-    fwMgt = ''
+def run_cmd(cmd):
+    subprocess.call('az login', shell=True)
+    res = subprocess.call(cmd, shell=True)
+    print ('Result is {}'.format(res))
 
-    # Set run_plan to TRUE is you wish to run terraform plan before apply
-    run_plan = False
-    deployment_status = {}
-    kwargs = {"auto-approve": True}
 
+def delete_file(fpath):
+    try:
+        os.remove(fpath)
+        print ('Removed state file {}'.format(fpath))
+    except Exception as e:
+        print ('Unable to delete the file {} got error {}'.format(fpath, e))
+
+def az_cli(args_str):
+    temp = tempfile.TemporaryFile()
+    args = args_str.split()
+    logger.debug('Sending cli command {}'.format(args))
+    code = get_default_cli().invoke(args, None, temp)
+    # temp.seek(0)
+    data = temp.read().strip()
+    temp.close()
+    return [code, data]
+
+def delete_rg(rg_name):
+    logger.info('Deleting resource group {}'.format(rg_name))
+    cmd = 'group delete --name ' + rg_name + ' --yes'
+    code, data = az_cli(cmd)
+    if code == 0:
+        print ('Successfully deleted Rg {} {}'.format(code,rg_name))
+
+def main (username, password):
+    #get_default_cli().invoke(['login', "--use-device-code"], out_file=sys.stdout)
     #
     # Destroy Infrastructure
     #
-    tf = Terraform(working_dir='./WebInDeploy')
-    rg_name = tf.output('RG_Name')
+    tfstate_file = 'terraform.tfstate'
+    fpath = './WebInDeploy/' + tfstate_file
+    if os.path.isfile(fpath):
+        tf = Terraform(working_dir='./WebInDeploy')
+        rg_name = tf.output('RG_Name')
+        rg_name1 = tf.output('Attacker_RG_Name')
+        delete_rg_cmd = 'group delete --name ' + rg_name + ' --yes'
+        az_cli(delete_rg_cmd)
+        delete_file(fpath)
 
-    attack_rg_name = tf.output('Attacker_RG_Name')
-    logger.info('Got RG_Name {} and Attacker_RG_Name {}'.format(rg_name, attack_rg_name))
-
-    WebInDeploy_vars.update({'RG_Name': rg_name})
-    WebInDeploy_vars.update({'Attack_RG_Name': attack_rg_name})
-
-    if run_plan:
-        print('Calling tf.plan')
-        tf.plan(capture_output=False)
-
-    return_code1, stdout, stderr = tf.cmd('destroy', var=WebInDeploy_vars, capture_output=False, **kwargs)
-    # return_code1 =0
-    print('Got return code {}'.format(return_code1))
-
-    if return_code1 != 0:
-        logger.info("Failed to destroy build ")
-
-        exit()
-    else:
-
-        logger.info("Destroyed WebInDeploy ")
-
-    WebInBootstrap_vars.update({'RG_Name': rg_name})
-    WebInBootstrap_vars.update({'Attack_RG_Name': attack_rg_name})
-
-    tf = Terraform(working_dir='./WebInBootstrap')
-
-    if run_plan:
-        print('Calling tf.plan')
-        tf.plan(capture_output=False)
-
-    return_code1, stdout, stderr = tf.cmd('destroy', var=WebInBootstrap_vars, capture_output=False, **kwargs)
-    # return_code1 =0
-    print('Got return code {}'.format(return_code1))
-
-    if return_code1 != 0:
-        logger.info("WebInBootstrap destroyed")
-        deployment_status = {'WebInDeploy': 'Fail'}
-
-        exit()
-    else:
-        deployment_status = {'WebInDeploy': 'Success'}
-        exit()
+    fpath = './WebInBootstrap/' + tfstate_file
+    if os.path.isfile(fpath):
+        delete_rg_cmd = 'group delete --name ' + rg_name1 + ' --yes'
+        az_cli(delete_rg_cmd)
+        delete_file(fpath)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get Terraform Params')
     parser.add_argument('-u', '--username', help='Firewall Username', required=True)
     parser.add_argument('-p', '--password', help='Firewall Password', required=True)
-
     args = parser.parse_args()
     username = args.username
     password = args.password
-
+    # get_default_cli().invoke(['login', "--use-device-code"], out_file=sys.stdout)
     main(username, password)
