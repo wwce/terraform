5b88dfc6fdf522abfb7f89ac2e00a652b9069b4f
franklinjff <32177920+franklinjff@users.noreply.github.com>
2019-05-02 15:13:39 -0600

    Update bootstrap.tf
    add random hex to bucket name

--- aws/Jenkins_proj-master/WebInDeploy/bootstrap.tf
+++ aws/Jenkins_proj-master/WebInDeploy/bootstrap.tf
@@ -1,7 +1,18 @@
 # Create a BootStrap S3 Bucket
 
+resource "random_id" "bucket_prefix" {
+  byte_length = 4
+}
+
+#data "aws_s3_bucket" "jenkins" {
+#  bucket = "bootstrap_bucket"
+
+#region = "${var.aws_region}"
+#}
+
 resource "aws_s3_bucket" "bootstrap_bucket" {
-  bucket        = "${var.bootstrap_s3bucket}"
+  #bucket_prefix = "${var.bucket_prefix}"
+  bucket        = "sec-frame-jenkins-${lower(random_id.bucket_prefix.hex)}"
   acl           = "private"
   force_destroy = true
 
@@ -10,38 +21,42 @@ resource "aws_s3_bucket" "bootstrap_bucket" {
   }
 }
 
-# Create Folders and Upload Bootstrap Files
 resource "aws_s3_bucket_object" "bootstrap_xml" {
-  bucket = "${aws_s3_bucket.bootstrap_bucket.id}"
-  acl    = "private"
-  key    = "config/bootstrap.xml"
-  source = "bootstrap/bootstrap.xml"
+  depends_on = ["aws_s3_bucket.bootstrap_bucket"]
+  bucket     = "sec-frame-jenkins-${lower(random_id.bucket_prefix.hex)}"
+  acl        = "private"
+  key        = "config/bootstrap.xml"
+  source     = "bootstrap/bootstrap.xml"
 }
 
 resource "aws_s3_bucket_object" "init-cft_txt" {
-  bucket = "${aws_s3_bucket.bootstrap_bucket.id}"
-  acl    = "private"
-  key    = "config/init-cfg.txt"
-  source = "bootstrap/init-cfg.txt"
+  bucket     = "sec-frame-jenkins-${lower(random_id.bucket_prefix.hex)}"
+  depends_on = ["aws_s3_bucket.bootstrap_bucket"]
+  acl        = "private"
+  key        = "config/init-cfg.txt"
+  source     = "bootstrap/init-cfg.txt"
 }
 
 resource "aws_s3_bucket_object" "software" {
-  bucket = "${aws_s3_bucket.bootstrap_bucket.id}"
-  acl    = "private"
-  key    = "software/"
-  source = "/dev/null"
+  bucket     = "sec-frame-jenkins-${lower(random_id.bucket_prefix.hex)}"
+  depends_on = ["aws_s3_bucket.bootstrap_bucket"]
+  acl        = "private"
+  key        = "software/"
+  source     = "/dev/null"
 }
 
 resource "aws_s3_bucket_object" "license" {
-  bucket = "${aws_s3_bucket.bootstrap_bucket.id}"
-  acl    = "private"
-  key    = "license/"
-  source = "/dev/null"
+  bucket     = "sec-frame-jenkins-${lower(random_id.bucket_prefix.hex)}"
+  depends_on = ["aws_s3_bucket.bootstrap_bucket"]
+  acl        = "private"
+  key        = "license/"
+  source     = "/dev/null"
 }
 
 resource "aws_s3_bucket_object" "content" {
-  bucket = "${aws_s3_bucket.bootstrap_bucket.id}"
-  acl    = "private"
-  key    = "content/"
-  source = "/dev/null"
+  bucket     = "sec-frame-jenkins-${lower(random_id.bucket_prefix.hex)}"
+  depends_on = ["aws_s3_bucket.bootstrap_bucket"]
+  acl        = "private"
+  key        = "content/"
+  source     = "/dev/null"
 }
