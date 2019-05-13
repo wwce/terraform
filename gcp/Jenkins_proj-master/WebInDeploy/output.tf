65ec910a15d3409796d7798b0ffa7a98c9761567
jharris10 <jharris@paloaltonetworks.com>
2019-05-13 17:27:06 +0100

    Fixed output.tf to include IP addresses for testing

--- gcp/Jenkins_proj-master/WebInDeploy/output.tf
+++ gcp/Jenkins_proj-master/WebInDeploy/output.tf
@@ -1,6 +1,12 @@
 output "FW_Mgmt_IP" {
   value = "${google_compute_instance.firewall.network_interface.1.access_config.0.nat_ip}"
 }
+
 output "ALB-DNS" {
-  value = "${google_compute_url_map.firewalls.self_link}"
+  value = "${google_compute_global_forwarding_rule.firewalls.ip_address}"
 }
+
+output "NATIVE-DNS" {
+  value = "${google_compute_global_forwarding_rule.webservers.ip_address}"
+}
+
