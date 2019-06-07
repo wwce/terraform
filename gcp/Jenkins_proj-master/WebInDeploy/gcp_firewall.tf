8eac913814d10100d586dfa599dc77f5edcd83eb
ptglynn <pglynn@paloaltonetworks.com>
2019-06-07 13:01:36 -0500

    Update gcp_firewall.tf
    Add 443 and 5000 to the attacker-firewall

--- gcp/Jenkins_proj-master/WebInDeploy/gcp_firewall.tf
+++ gcp/Jenkins_proj-master/WebInDeploy/gcp_firewall.tf
@@ -29,6 +29,6 @@ resource "google_compute_firewall" "attacker" {
   network = "${google_compute_network.attacker_network.name}"
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["22", "443", "5000"]
   }
 }
