71b62003ffa3413cefad82332420b161a3b1407c
ptglynn <pglynn@paloaltonetworks.com>
2019-06-18 15:07:22 -0500

    remove deprecated create_timeout

--- gcp/Jenkins_proj-master/WebInDeploy/attackers.tf
+++ gcp/Jenkins_proj-master/WebInDeploy/attackers.tf
@@ -4,7 +4,6 @@ resource "google_compute_instance" "attacker" {
   zone         = "${var.GCP_Zone}"
   machine_type = "n1-standard-1"
   allow_stopping_for_update = true
-  create_timeout = 8
   depends_on = [
                 "google_storage_bucket_object.config_file_attacker",
                 "google_project_service.attacker_project"
