03d6f2a698d429d1b0def39250550e576d12830c
franklinjff <franklinjff@gmail.com>
2019-06-20 16:43:56 -0600

    add attacker container dockerhub

--- aws/Jenkins_proj-master/WebInDeploy/kali-instance.tf
+++ aws/Jenkins_proj-master/WebInDeploy/kali-instance.tf
@@ -28,18 +28,11 @@ resource "aws_instance" "kali" {
 
   user_data = "${base64encode(join("", list(
       "#! /bin/bash\n",
-          "sudo su\n",
-          "apt-get update\n",
-          "apt install docker.io python3-pip build-essential libssl-dev libffi-dev -y --force-yes\n",
-          "pip3 install docker-compose\n",
-          "cd /var/tmp\n",
-          "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/.temp/Dockerfile\n",
-          "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/.temp/docker-compose.yml\n",
-          "wget https://github.com/wwce/terraform/blob/master/aws/Jenkins_proj-master/attacker/run.sh\n",
-          "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/attacker/auto-sploit.sh\n",
-          "wget https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/exp-server.py\n",
-          "docker-compose build\n",
-          "docker-compose up -d\n"
+      "#! /bin/bash\n",
+             "sudo cd /var/tmp\n",
+             "sudo wget -O initialize_attacker.sh https://raw.githubusercontent.com/wwce/terraform/master/aws/Jenkins_proj-master/WebInDeploy/scripts/initialize_attacker.sh\n",
+             "sudo chmod 755 initialize_attacker.sh &&\n",
+             "sudo bash ./initialize_attacker.sh
     )))
    }"
 }
