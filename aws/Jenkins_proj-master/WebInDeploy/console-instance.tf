8cdf3d5b128b0b3b8cf3788cef7b6eab8f4f0f7f
jharris10 <jharris@paloaltonetworks.com>
2019-09-15 18:25:19 +0100

    Added console install via user-data

---
+++ aws/Jenkins_proj-master/WebInDeploy/console-instance.tf
@@ -0,0 +1,40 @@
+resource "aws_network_interface" "console-int" {
+  subnet_id         = "${aws_subnet.AZ1-MGT.id}"
+  security_groups   = ["${aws_security_group.sgWideOpen.id}"]
+  source_dest_check = false
+  private_ips       = ["10.0.0.10"]
+}
+
+resource "aws_eip_association" "console-Association" {
+  network_interface_id = "${aws_network_interface.console-int.id}"
+  allocation_id        = "${aws_eip.console.id}"
+}
+
+resource "aws_instance" "twistlock-console" {
+  # instance_initiated_shutdown_behavior = "stop"
+  ami           = "${var.UbuntuRegionMap[var.aws_region]}"
+  instance_type = "t2.xlarge"
+  key_name      = "${var.ServerKeyName}"
+  monitoring    = false
+
+  tags {
+    Name = "twistlock-console-AZ1"
+  }
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = "${aws_network_interface.console-int.id}"
+  }
+  root_block_device {
+      volume_size = 30
+  }
+
+  user_data = "${base64encode(join("", list(
+   "#! /bin/bash\n",
+          "sudo cd /var/tmp\n",
+          "sudo wget -O initialize_webserver.sh https://raw.githubusercontent.com/wwce/terraform/twistlck/aws/Jenkins_proj-master/WebInDeploy/scripts/initialize_console.sh\n",
+          "sudo chmod 755 initialize_console.sh &&\n",
+          "sudo bash ./initialize_console.sh\n"
+   )))
+   }"
+}
--- aws/Jenkins_proj-master/WebInDeploy/public_ips.tf
+++ aws/Jenkins_proj-master/WebInDeploy/public_ips.tf
@@ -7,10 +7,10 @@ resource "aws_eip" "FW1-MGT" {
   vpc   = true
   depends_on = ["aws_vpc.main", "aws_internet_gateway.gw"]
 }
-#resource "aws_eip" "webserver" {
-#  vpc   = true
-#  depends_on = ["aws_vpc.main", "aws_internet_gateway.gw"]
-#}
+resource "aws_eip" "console" {
+  vpc   = true
+  depends_on = ["aws_vpc.main", "aws_internet_gateway.gw"]
+}
 resource "aws_eip" "NAT_GW" {
   vpc   = true
   depends_on = ["aws_vpc.main", "aws_internet_gateway.gw"]
