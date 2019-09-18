28135d74067764729e03935a90a044ea46aa0638
jharris10 <jharris@paloaltonetworks.com>
2019-09-18 21:16:05 +0200

    Added console install via user-data

--- aws/Jenkins_proj-master/WebInDeploy/console-instance.tf
+++ aws/Jenkins_proj-master/WebInDeploy/console-instance.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "twistlock-console" {
   user_data = "${base64encode(join("", list(
    "#!/bin/bash\n",
           "sudo cd /var/tmp\n",
-          "sudo wget -O initialize_console.sh <cdn-url>",
+          "sudo wget -O initialize_console.sh https://raw.githubusercontent.com/wwce/terraform/twistlck/aws/Jenkins_proj-master/WebInDeploy/scripts/initialise_console.sh\n",
           "sudo chmod 755 /var/tmp/initialize_console.sh &&\n",
           "sudo bash /var/tmp/initialize_console.sh <cdn-url>\n"
    )))
