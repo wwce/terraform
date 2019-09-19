be983452cd28b2045a1aa09f0c25de2cbaaac8f7
jharris10 <jharris@paloaltonetworks.com>
2019-09-19 21:16:58 +0200

    Updated sg-kali with inbound ports for Twistlock

--- aws/Jenkins_proj-master/WebInDeploy/kali-ACL-SG.tf
+++ aws/Jenkins_proj-master/WebInDeploy/kali-ACL-SG.tf
@@ -44,6 +44,12 @@ resource "aws_security_group" "kaliWideOpen" {
     cidr_blocks = [
       "0.0.0.0/0"]
   }
+  ingress {
+    from_port = "8083"
+    to_port = "8083"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = "5000"
