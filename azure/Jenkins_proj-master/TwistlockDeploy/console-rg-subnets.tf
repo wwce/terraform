46d4e066be783b724ba05a768b0d5cbb64901c5e
ptglynn <pglynn@paloaltonetworks.com>
2019-10-05 14:58:01 -0500

    Update console-rg-subnets.tf
    more case errors

--- azure/Jenkins_proj-master/TwistlockDeploy/console-rg-subnets.tf
+++ azure/Jenkins_proj-master/TwistlockDeploy/console-rg-subnets.tf
@@ -5,7 +5,7 @@ resource "random_id" "console_resource_group" {
   byte_length = 2
 }
 resource "azurerm_resource_group" "consolegroup" {
-	name		= "${var.console_RG_Name}-${lower(random_id.console_resource_group.hex)}"
+	name		= "${var.Console_RG_Name}-${lower(random_id.console_resource_group.hex)}"
 	location	= "${var.Azure_Region}"
 }
 
@@ -25,5 +25,5 @@ resource "azurerm_subnet" "console" {
   name                 = "console"
   resource_group_name  = "${azurerm_resource_group.consolegroup.name}"
   virtual_network_name = "${azurerm_virtual_network.console-vnet.name}"
-  address_prefix       = "${var.console_Subnet_CIDR}"
+  address_prefix       = "${var.Console_Subnet_CIDR}"
 }
