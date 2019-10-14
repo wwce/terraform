1a0eb0433bcf1881f0d4e9a95332a9203e92a24f
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2019-10-13 19:48:02 -0700

    Add files via upload

---
+++ gcp/adv_peering_4fw_transit/v12/terraform.tfvars
@@ -0,0 +1,41 @@
+#ssh_key     = ""
+#project_id  = ""
+fw_panos    = "byol-819"
+#fw_panos   = "bundle1-819" 
+#fw_panos   = "bundle2-819"
+
+#-------------------------------------------------------------------
+region            = "us-east4"
+zones             = ["us-east4-a", "us-east4-b"]
+
+mgmt_vpc          = "mgmt"
+mgmt_subnet       = ["mgmt"]
+mgmt_cidr         = ["192.168.0.0/24"]
+mgmt_sources      = ["0.0.0.0/0"]
+
+untrust_vpc       = "untrust"
+untrust_subnet    = ["untrust"]
+untrust_cidr      = ["192.168.1.0/24"]
+
+trust_vpc         = "trust"
+trust_subnet      = ["trust"]
+trust_cidr        = ["192.168.2.0/24"]
+
+spoke1_vpc        = "spoke1"
+spoke1_subnets    = ["spoke1-web"]
+spoke1_cidrs      = ["10.10.1.0/24"]
+spoke1_vms        = ["spoke1-vm1", "spoke1-vm2"]
+spoke1_ilb        = "spoke1-ilb"
+spoke1_ilb_ip     = "10.10.1.100"
+
+spoke2_vpc        = "spoke2"
+spoke2_subnets    = ["spoke2-db"]
+spoke2_cidrs      = ["10.10.2.0/24"]
+spoke2_vms        = ["spoke2-vm1"]
+
+fw_names_inbound  = ["vmseries01", "vmseries02"]
+fw_names_outbound = ["vmseries03", "vmseries04"]
+fw_machine_type   = "n1-standard-4"
+
+glb_name          = "vmseries-glb"
+ilb_name          = "vmseries-ilb"
---
+++ gcp/adv_peering_4fw_transit/v12/variables.tf
@@ -0,0 +1,128 @@
+provider "google" {
+  #credentials = auth_file.json
+  project     = var.project_id
+  region      = var.region
+}
+
+provider "google-beta" {
+  #credentials = auth_file.json
+  project     = var.project_id
+  region      = var.region
+  version     = "> 2.13.0"
+}
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+variable "project_id" {
+  description = "GCP Project ID"
+}
+
+variable "region" {
+}
+
+variable "zones" {
+  type = list(string)
+}
+variable "ssh_key" {
+  description = "SSH key"
+}
+
+variable "fw_panos" {
+  description = "VM-Series license and PAN-OS (ex: bundle1-819, bundle2-819, byol-819)"
+}
+
+variable "fw_image" {
+  default = "https://www.googleapis.com/compute/v1/projects/paloaltonetworksgcp-public/global/images/vmseries"
+}
+
+variable "fw_names_inbound" {
+  type = list(string)
+}
+
+variable "fw_names_outbound" {
+  type = list(string)
+}
+
+variable "fw_machine_type" {
+}
+
+variable "glb_name" {
+}
+
+variable "ilb_name" {
+}
+
+variable "mgmt_vpc" {
+}
+
+variable "mgmt_subnet" {
+  type = list(string)
+}
+
+variable "mgmt_cidr" {
+  type = list(string)
+}
+
+variable "untrust_vpc" {
+}
+
+variable "untrust_subnet" {
+  type = list(string)
+}
+
+variable "untrust_cidr" {
+  type = list(string)
+}
+
+variable "trust_vpc" {
+}
+
+variable "trust_subnet" {
+  type = list(string)
+}
+
+variable "trust_cidr" {
+  type = list(string)
+}
+
+variable "mgmt_sources" {
+  type = list(string)
+}
+
+variable "spoke1_vpc" {
+}
+
+variable "spoke1_subnets" {
+  type = list(string)
+}
+
+variable "spoke1_cidrs" {
+  type = list(string)
+}
+
+variable "spoke1_vms" {
+  type = list(string)
+}
+
+variable "spoke1_ilb" {
+}
+
+variable "spoke1_ilb_ip" {
+}
+
+variable "spoke2_vpc" {
+}
+
+variable "spoke2_subnets" {
+  type = list(string)
+}
+
+variable "spoke2_cidrs" {
+  type = list(string)
+}
+
+variable "spoke2_vms" {
+  type = list(string)
+}
