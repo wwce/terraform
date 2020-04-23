5f96a2ab562515d76820c49a14a0250d2f270a49
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2020-04-23 13:14:49 -0400

    Update webserver-startup.sh

--- gcp/adv_peering_2fw_2spoke_common/scripts/webserver-startup.sh
+++ gcp/adv_peering_2fw_2spoke_common/scripts/webserver-startup.sh
@@ -6,3 +6,4 @@ until sudo apt-get install -y libapache2-mod-php; do echo "Retrying"; sleep 2; d
 until sudo rm -f /var/www/html/index.html; do echo "Retrying"; sleep 2; done
 until sudo wget -O /var/www/html/index.php https://raw.githubusercontent.com/wwce/terraform/master/gcp/adv_peering_4fw_2spoke/scripts/showheaders.php; do echo "Retrying"; sleep 2; done
 until sudo systemctl restart apache2; do echo "Retrying"; sleep 2; done
+sudo reboot
