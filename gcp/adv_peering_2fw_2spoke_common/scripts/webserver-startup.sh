0264e7f4219bf47a49f2e4a0e5c99b04d427f87a
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2020-04-23 13:42:47 -0400

    Update webserver-startup.sh

--- gcp/adv_peering_2fw_2spoke_common/scripts/webserver-startup.sh
+++ gcp/adv_peering_2fw_2spoke_common/scripts/webserver-startup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sleep 120;
-until sudo apt-get update; do echo "Retrying"; sleep 2; done
-until sudo apt-get install -y php; do echo "Retrying"; sleep 2; done
-until sudo apt-get install -y apache2; do echo "Retrying"; sleep 2; done
-until sudo apt-get install -y libapache2-mod-php; do echo "Retrying"; sleep 2; done
-until sudo rm -f /var/www/html/index.html; do echo "Retrying"; sleep 2; done
+until sudo apt-get update; do echo "Retrying"; sleep 5; done
+until sudo apt-get install -y php; do echo "Retrying"; sleep 5; done
+until sudo apt-get install -y apache2; do echo "Retrying"; sleep 5; done
+until sudo apt-get install -y libapache2-mod-php; do echo "Retrying"; sleep 5; done
+until sudo rm -f /var/www/html/index.html; do echo "Retrying"; sleep 5; done
 until sudo wget -O /var/www/html/index.php https://raw.githubusercontent.com/wwce/terraform/master/gcp/adv_peering_2fw_2spoke_common/scripts/showheaders.php; do echo "Retrying"; sleep 2; done
-until sudo systemctl restart apache2; do echo "Retrying"; sleep 2; done
-sudo reboot
+until sudo systemctl restart apache2; do echo "Retrying"; sleep 5; done
