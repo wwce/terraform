331473e607850ad5bd54a9a43b71bbfa6f33fd09
ptglynn <pglynn@paloaltonetworks.com>
2020-07-27 18:35:39 -0500

    Add files via upload
    Update var file to deploy 9.0.9

--- aws/Jenkins-sans-Panhandler/aws_vars.tf
+++ aws/Jenkins-sans-Panhandler/aws_vars.tf
@@ -17,6 +17,7 @@ variable "server_initscript_path" {}
 variable "fw1_mgmt_ip" {}
 variable "fw1_untrust_ip" {}
 variable "fw1_trust_ip" {}
+variable "content_bucket" {}
 
 variable "red_team_cidr" {}
 variable "red_team_ip" {}
@@ -27,20 +28,21 @@ variable "PANFWRegionMap" {
   type = "map"
 
   default = {
-    "us-east-1"      = "ami-0c21b9d4f6c321d91"
-    "us-east-2"      = "ami-0b4118f66d7fd498a"
-    "us-west-1"      = "ami-0ced18b853bbdb1af"
-    "us-west-2"      = "ami-0ced18b853bbdb1af"
-    "eu-west-1"      = "ami-0a87d6cfb1c824cf3"
-    "eu-west-2"      = "ami-0af0ac47477365eb4"
-    "ap-northeast-1" = "ami-0f6a57f775518a126"
-    "ap-northeast-2" = "ami-00024db5e2d44505b"
-    "ap-southeast-1" = "ami-073fb55a53ffd2349"
-    "ap-southeast-2" = "ami-0dba20ade080c37a5"
-    "eu-central-1"   = "ami-02412a8d1d7b17aca"
-    "sa-east-1"      = "ami-0650b573565b1283d"
-    "ca-central-1"   = "ami-0f3a34c56770ea7d5"
-    "ap-south-1"     = "ami-06024425a7931a975"
+    "ap-northeast-1"  = "ami-0c2df2ad98fa740f2"
+    "ap-northeast-2"  = "ami-0644fb0e02257e3e0"
+    "ap-south-1"      = "ami-0f03c2e41c35f4155"
+    "ap-southeast-1"  = "ami-0c18d932bcd6eb39f"
+    "ap-southeast-2"  = "ami-0b6f63cee6e82b6b1"
+    "ca-central-1"    = "ami-09ccdba2948398b45"
+    "eu-central-1"    = "ami-0cf748c2bf505e674"
+    "eu-north-1"      = "ami-0b5ba14c2b9ccb9b6"
+    "eu-west-1"       = "ami-02eec133b97eaa1e1"
+    "eu-west-2"       = "ami-0c06868bcef1d35ba"
+    "sa-east-1"       = "ami-08a812daea4ede4a2"
+    "us-east-1"       = "ami-07c7f3e5ce94b86cd"
+    "us-east-2"       = "ami-05025aa19e768421d"
+    "us-west-1"       = "ami-0ba71b23bb2d76fa1"
+    "us-west-2"       = "ami-074c89c1e945ede8a"
   }
 }
 
