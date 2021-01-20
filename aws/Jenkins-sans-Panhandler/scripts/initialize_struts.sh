70db84e8cf255affb24d77cd5f60a787df887d12
ptglynn <pglynn@paloaltonetworks.com>
2021-01-20 11:11:03 -0600

    Add files via upload
    Added server init scripts for struts exploit demo.

---
+++ aws/Jenkins-sans-Panhandler/scripts/initialize_struts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get update
+apt install docker.io python3-pip build-essential libssl-dev libffi-dev docker-compose -y --force-yes
+cd /var/tmp
+echo "version: '2'" > docker-compose.yml
+echo "services:" >> docker-compose.yml
+echo "  struts:" >> docker-compose.yml
+echo "    image: pglynn/apache-struts2-demo:latest" >> docker-compose.yml
+echo "    ports:" >> docker-compose.yml
+echo "      - \"8080:8080\"" >> docker-compose.yml
+docker-compose up -d
