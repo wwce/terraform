1c42b6e53806363ad53a71cdd8c7d2963eeff65b
ptglynn <pglynn@paloaltonetworks.com>
2021-05-06 16:36:10 -0500

    Add files via upload
    Update initialization script to mount pre-configured environment.

--- aws/PANW-Honeypot-Demo/scripts/initialize_cowrie.sh
+++ aws/PANW-Honeypot-Demo/scripts/initialize_cowrie.sh
@@ -10,11 +10,10 @@ done
 apt-get update
 apt-get update
 apt install docker.io python3-pip build-essential libssl-dev libffi-dev docker-compose -y --force-yes
+wget --no-check-certificate https://security-framework-us-west-2.s3.amazonaws.com/cowrie_files_20210504.tar.gz
+tar zxf cowrie_files_20210504.tar.gz
 cd /var/tmp
 echo "version: '2'" > docker-compose.yml
-echo "volumes:" >> docker-compose.yml
-echo "  cowrie-etc:" >> docker-compose.yml
-echo "  cowrie-var:" >> docker-compose.yml
 echo "services:" >> docker-compose.yml
 echo "  cowrie:" >> docker-compose.yml
 echo "    restart: always" >> docker-compose.yml
@@ -25,7 +24,8 @@ echo "    ports:" >> docker-compose.yml
 echo "      - \"2222:2222\"" >> docker-compose.yml
 echo "      - \"2223:2223\"" >> docker-compose.yml
 echo "    volumes:" >> docker-compose.yml
-echo "      - cowrie-etc:/cowrie/cowrie-git/etc" >> docker-compose.yml
-echo "      - cowrie-var:/cowrie/cowrie-git/var" >> docker-compose.yml
+echo "      - /home/cowrie/cowrie/etc:/cowrie/cowrie-git/etc" >> docker-compose.yml
+echo "      - /home/cowrie/cowrie/share:/cowrie/cowrie-git/share" >> docker-compose.yml
+echo "      - /home/cowrie/cowrie/honeyfs:/cowrie/cowrie-git/honeyfs" >> docker-compose.yml
 docker-compose up -d
 usermod -aG docker ubuntu
