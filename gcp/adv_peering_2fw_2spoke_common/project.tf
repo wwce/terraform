bda77727132f6bca54b07e90a68872c8ce192809
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2021-05-13 13:07:42 -0400

    Update project.tf

--- gcp/adv_peering_2fw_2spoke_common/project.tf
+++ gcp/adv_peering_2fw_2spoke_common/project.tf
@@ -8,11 +8,4 @@ provider "google" {
   region      = var.regions[0]
 }
 
-provider "google-beta" {
-  #credentials = var.auth_file
-  project     = var.project_id
-  region      = var.regions[0]
-  version     = "> 3.0.0"
-}
-
 data "google_compute_zones" "available" {}
