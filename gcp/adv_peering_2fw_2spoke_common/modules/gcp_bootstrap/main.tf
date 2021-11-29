70411861da0e252a2f3daf11310110498df8adcc
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2021-11-29 12:03:05 -0500

    Update main.tf

--- gcp/adv_peering_2fw_2spoke_common/modules/gcp_bootstrap/main.tf
+++ gcp/adv_peering_2fw_2spoke_common/modules/gcp_bootstrap/main.tf
@@ -11,7 +11,7 @@ resource "random_string" "randomstring" {
 resource "google_storage_bucket" "bootstrap" {
   name          = local.bucket_name
   force_destroy = true
-  location      = var.region
+  location      = var.location
 }
 
 resource "google_storage_bucket_object" "config_full" {
