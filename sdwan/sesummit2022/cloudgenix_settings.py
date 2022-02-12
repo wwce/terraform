d8fcac08703268b2553cb20eb8b79a6a7f816542
ptglynn <pglynn@paloaltonetworks.com>
2022-02-11 18:11:58 -0600

    Add files via upload
    Add files

---
+++ sdwan/sesummit2022/cloudgenix_settings.py
@@ -0,0 +1,10 @@
+### Enter user/password below, or comment out user/password, uncomment and enter AUTH_TOKEN below.
+### Additionally, scripts can take AUTH_TOKEN from X_AUTH_TOKEN or AUTH_TOKEN Environment Variables.
+###
+
+#CLOUDGENIX_USER = ''
+#CLOUDGENIX_PASSWORD = ''
+
+### Instead of CLOUDGENIX_USER or CLOUDGENIX_PASSWORD, you can set CLOUDGENIX_AUTH_TOKEN instead.
+
+CLOUDGENIX_AUTH_TOKEN = '<your auth token here>'
