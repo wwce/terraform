3879fb6c2a0426e95502c25e9377e14883de2d02
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2022-08-29 15:40:18 -0400

    Update providers.tf

--- azure/transit_2fw_2spoke_common/providers.tf
+++ azure/transit_2fw_2spoke_common/providers.tf
@@ -8,9 +8,5 @@ provider "azurerm" {
 #  client_id       = var.client_id
 #  client_secret   = var.client_secret
 #  tenant_id       = var.tenant_id
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
